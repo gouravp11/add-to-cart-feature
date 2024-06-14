c7c9e1134b4f53610d8746c7c50a93a5c37bc197
recondite11 <kartikpal1402@gmail.com>
2024-06-14 14:17:40 +0530

    Can add more quantity of items by pr

--- script.js
+++ script.js
@@ -3,150 +3,149 @@ var products = [
   {
     img: "https://images.unsplash.com/photo-1625772452859-1c03d5bf1137?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "moisturizer",
-    price: 10,
+    price: 1,
     id: 0
   },
   {
     img: "https://images.unsplash.com/photo-1491553895911-0055eca6402d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "nike shoes",
-    price: 40
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
     name: "sunglasses",
-    price: 30
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1525904097878-94fb15835963?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "mayonaise",
-    price: 15
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1627384113743-6bd5a479fffd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "face cream",
-    price: 25
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
   {
     img: "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3RzfGVufDB8fDB8fHww",
     name: "perfume",
-    price: 19
+    price: 1
   },
 ];
 
 // Utility Functions
 function capitalizer(str) {
-  var strArr = str.split(" ");
-  var capitalizedArr = strArr.map(function (word) {
+  var capitalizedArr = str.split(" ").map(function (word) {
     return word[0].toUpperCase() + word.slice(1);
   });
   var capitalizedStr = capitalizedArr.join(" ");
@@ -192,22 +191,51 @@ function cartToggler() {
 }
 cartToggler();
 
-function addToCart() {
+
+function updateProductCount(count){
+  document.querySelector("#product-count").innerHTML = count;
+}
+  var cartProducts=[];
+  var i, quantity;
   var cart = [];
   var prices = [];
   var totalPrice;
-  var cartProduct;
-  var cartCount = 0;
-  var quantity = 1;
-  var container = document.createElement('div');
+  var productCount = 0;
+  // var quantity;
+function addToCart() {
+  var keyExists;
+  var objIdx;
+  var cartContent = document.querySelector(".cart-content");
   document.querySelector('#products').addEventListener('click', function (dets) {
     if (dets.target.classList.contains('add-to-cart')) {
-      var i = dets.target.dataset.index;
-      var addBtn = document.querySelector(`#add-${i}`)
-      if (dets.target.innerHTML == "Add to cart") {
-        addBtn.classList.add('added');
-        addBtn.innerHTML = "Added";
-        container.innerHTML = `<div id="pro-${i}" class="added-product">
+      i = Number(dets.target.dataset.index);
+      for(let j=0; j<cartProducts.length; j++){
+        if(cartProducts[j].index==i){
+          keyExists= true
+          objIdx= j;
+          break;
+        }
+        else{
+          keyExists= false
+        }
+      }
+      // console.log(keyExists)
+      if(keyExists){
+        if(cartProducts[objIdx].quantity<10){
+          var q= ++cartProducts[objIdx].quantity;
+          document.querySelector(`#quan-${i}`).innerHTML = q;
+          // console.log(cartProducts)
+          prices.push(products[i].price);
+          // console.log(prices)
+          totalPrice = prices.reduce((curr, prev) => curr + prev);
+          document.querySelector("#total").innerHTML = totalPrice;
+        }
+        else{
+          alert("Quantity cannot exceed 10!");
+        }
+      }
+      else{
+        cartContent.innerHTML += `<div id="pro-${i}" class="added-product">
         <div class="product-info">
             <img src="${products[i].img}"
                 alt="img not loaded">
@@ -219,70 +247,55 @@ function addToCart() {
         <div class="quantity">
             <i id="dec-${i}" class="ri-subtract-line"></i><span id="quan-${i}">1</span><i id="inc-${i}"class="ri-add-line"></i>
         </div>
-    </div>`;
-        cartProduct = container.children[0];
-        cart.push(cartProduct)
+        </div>`;
+        quantity= Number(document.querySelector(`#quan-${i}`).textContent);
+        // console.log(quantity);
+        productCount++;
+        updateProductCount(productCount);
+        cartProducts.push({'index': i, 'quantity':quantity});
+        // console.log(cartProducts)
         prices.push(products[i].price);
+        // console.log(prices)
         totalPrice = prices.reduce((curr, prev) => curr + prev);
-        cartCount += 1;
-        document.querySelector("#cart-count").innerHTML = cartCount;
-        document.querySelector(".cart-content").appendChild(cartProduct);
-        document.querySelector("#total").innerHTML = totalPrice;
-
-        document.querySelector(`#dec-${i}`).addEventListener('click', function () {
-          quantity = parseInt(document.querySelector(`#quan-${i}`).innerHTML);
-          if (quantity > 1) {
-            quantity -= 1;
-            document.querySelector(`#quan-${i}`).innerHTML = quantity;
-          }
-          else{
-            addBtn.classList.remove('added');
-            addBtn.innerHTML= "Add to cart";
-            cartCount-=1
-            document.querySelector("#cart-count").innerHTML = cartCount;
-            cart.splice(findIndexById(cart, `pro-${i}`), 1);
-            document.querySelector(`#pro-${i}`).remove();
-
-            if (prices.length > 1) {
-              prices.splice(prices.indexOf(products[i].price), 1);
-              totalPrice = prices.reduce((curr, prev) => curr + prev);
-            }
-            else {
-              prices.splice(prices.indexOf(products[i].price), 1);
-              totalPrice = 0;
-            }
-            document.querySelector("#total").innerHTML = totalPrice;
-          }
-        })
-        document.querySelector(`#inc-${i}`).addEventListener('click', function () {
-          quantity = parseInt(document.querySelector(`#quan-${i}`).innerHTML);
-          if (quantity < 10) {
-            quantity += 1;
-          }
-          else {
-            alert("Quantity cannot be more than 10!")
-          }
-          document.querySelector(`#quan-${i}`).innerHTML = quantity;
-        })
-      }
-      else {
-        addBtn.innerHTML = "Add to cart"
-        addBtn.classList.remove('added');
-        cart.splice(findIndexById(cart, `pro-${i}`), 1);
-        if (prices.length > 1) {
-          prices.splice(prices.indexOf(products[i].price), 1);
-          totalPrice = prices.reduce((curr, prev) => curr + prev);
-        }
-        else {
-          prices.splice(prices.indexOf(products[i].price), 1);
-          totalPrice = 0;
-        }
-        cartCount -= 1
-        document.querySelector("#cart-count").innerHTML = cartCount;
-        document.querySelector(`#pro-${i}`).remove();
         document.querySelector("#total").innerHTML = totalPrice;
       }
     }
   })
 }
 addToCart();
+
+function cartModifier(){
+  document.querySelector(`#dec-${i}`).addEventListener('click', function () {
+    quantity = parseInt(document.querySelector(`#quan-${i}`).innerHTML);
+    if (quantity > 1) {
+      quantity -= 1;
+      document.querySelector(`#quan-${i}`).innerHTML = quantity;
+    }
+    else {
+      cartCount -= 1
+      document.querySelector("#cart-count").innerHTML = cartCount;
+      cart.splice(findIndexById(cart, `pro-${i}`), 1);
+      document.querySelector(`#pro-${i}`).remove();
+  
+      if (prices.length > 1) {
+        prices.splice(prices.indexOf(products[i].price), 1);
+        totalPrice = prices.reduce((curr, prev) => curr + prev);
+      }
+      else {
+        prices.splice(prices.indexOf(products[i].price), 1);
+        totalPrice = 0;
+      }
+      document.querySelector("#total").innerHTML = totalPrice;
+    }
+  })
+  document.querySelector(`#inc-${i}`).addEventListener('click', function () {
+    quantity = parseInt(document.querySelector(`#quan-${i}`).innerHTML);
+    if (quantity < 10) {
+      quantity += 1;
+    }
+    else {
+      alert("Quantity cannot be more than 10!")
+    }
+    document.querySelector(`#quan-${i}`).innerHTML = quantity;
+  })
+}
